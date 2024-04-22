0f41223cfc516561f3381af395938208b6545b76
Dari Parruca <parrucadari@gmail.com>
2024-04-23 00:21:05 +0200

    End of section 3

--- DattingApp/Program.cs
+++ DattingApp/Program.cs
@@ -12,9 +12,13 @@ opt.UseSqlServer(builder.Configuration.GetConnectionString("DatingAppDb")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
